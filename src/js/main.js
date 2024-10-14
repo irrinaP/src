ff36010c8d9756ce55b8761aa27ebf06210a2147
irrinaP <121332930+irrinaP@users.noreply.github.com>
2024-10-14 21:35:22 +0700

    deleteTask(numberTask), share(), cancel()

--- src/js/main.js
+++ src/js/main.js
@@ -17,6 +17,7 @@ function modal(action) {
     }
 }
 
+let numberTask = 0;
 function newTask(inputTitle, inputAbout) {
     let tasksCards = document.getElementsByClassName("tasks-cards")[0];
     let taskCard = document.createElement("div");
@@ -37,20 +38,22 @@ function newTask(inputTitle, inputAbout) {
     let taskActionEditImg = document.createElement("img");
 
     taskCard.classList.add("task-card");
+    taskCard.setAttribute("id", numberTask);
 
     taskInfo.classList.add("task-info");
-    taskInfo.setAttribute("onclick", "viewAction()");
+    taskInfo.setAttribute("onclick", "viewAction(" + numberTask + ")");
     taskText.classList.add("task-text");
     taskTextH1.innerText = inputTitle;
     taskTextP.innerText = inputAbout;
     taskButton.classList.add("task-button");
+    taskButton.setAttribute("onclick", "deleteTask(" + numberTask + ")");
     taskButtonAdd.innerHTML = "<span class='material-symbols-outlined'>add</span>";
 
     taskAction.classList.add("task-action");
     taskActionShare.setAttribute("onclick", "share()");
     taskActionShareImg.setAttribute("src", "icons/share.png");
     taskActionInfoImg.setAttribute("src", "icons/i.png");
-    taskActionEdit.setAttribute("onclick", "edit()");
+    taskActionEdit.setAttribute("onclick", "editView(" + ++numberTask + ")");
     taskActionEditImg.setAttribute("src", "icons/edit.png");
 
     taskText.append(taskTextH1);
@@ -69,32 +72,49 @@ function newTask(inputTitle, inputAbout) {
     tasksCards.append(taskCard);
 }
 
-function viewAction() {
-    let task = document.getElementsByClassName("task-card");
-    for (let i = 0; i < task.length; i++) {
-        task[i].getElementsByClassName("task-info")[0].onclick = () => {
-            let el = task[i].getElementsByClassName("task-action")[0];
-            let status = window.getComputedStyle(el).display;
+function deleteTask(numberTask) {
+    let task = document.getElementById(numberTask);
     
-            if (status == "none") {
-                el.style.display = "flex";
-                el.style.animation = "taskActionView 1s 1";
-            } else {
-                el.style.animation = "taskActionClose 1s 1";
-                setTimeout(() => {
-                    el.style.display = "none";
-                }, 900);
-            }
+    task.remove();
+    localStorage.removeItem(numberTask++);
+}
+
+function viewAction(numberTask) {
+    let task = document.getElementById(numberTask);
+    
+    task.getElementsByClassName("task-info")[0].onclick = () => {
+        let el = task.getElementsByClassName("task-action")[0];
+        let status = window.getComputedStyle(el).display;
+
+        if (status == "none") {
+            el.style.display = "flex";
+            el.style.animation = "taskActionView 1s 1";
+        } else {
+            el.style.animation = "taskActionClose 1s 1";
+            setTimeout(() => {
+                el.style.display = "none";
+            }, 900);
         }
     }
 }
 
+let isShare = false;
+let isEdit = false;
 function share() {
     modal("share");
+    isShare = true;
 }
 
-function edit() {
+function editView(numberTask) {
     modal("edit");
+
+    let titleText = JSON.parse(localStorage.getItem(numberTask)).title;
+    let aboutText = JSON.parse(localStorage.getItem(numberTask)).about;
+    let btnEdit = document.getElementsByClassName("modal-edit")[0].getElementsByClassName("modal-buttons")[0].getElementsByTagName("button")[1];
+
+    document.getElementsByClassName("modal-edit")[0].getElementsByTagName("input")[0].setAttribute("placeholder", titleText);
+    document.getElementsByClassName("modal-edit")[0].getElementsByTagName("textarea")[0].setAttribute("placeholder", aboutText);
+    btnEdit.setAttribute("id", numberTask);
 }
 
 function cancel() {
@@ -111,23 +131,75 @@ window.onload = () => {
 
     let numberTask = 0
     let btnAdd = document.getElementById("add");
+    let modal = document.getElementsByClassName("modal")[0];
+    let modalShare = modal.getElementsByClassName("modal-share")[0];
+    let modalEdit = modal.getElementsByClassName("modal-edit")[0];
+    let btnEdit = modalEdit.getElementsByClassName("modal-buttons")[0].getElementsByTagName("button")[1];
 
     btnAdd.onclick = () => {
         let header = document.getElementsByClassName("header")[0];
-        let inputTitle = header.getElementsByTagName("input")[0].value;
-        let inputAbout = header.getElementsByTagName("input")[1].value;
+        let inputTitle = header.getElementsByTagName("input")[0];
+        let inputAbout = header.getElementsByTagName("input")[1];
 
-        let tasks = document.getElementsByClassName("tasks")[0];
-        let tasksInfo = tasks.getElementsByClassName("tasks-info")[0];
-        let tasksCards = tasks.getElementsByClassName("tasks-cards")[0];
+        if (inputTitle.value != "" && inputAbout.value != "") {
+            let tasks = document.getElementsByClassName("tasks")[0];
+            let tasksInfo = tasks.getElementsByClassName("tasks-info")[0];
+            let tasksCards = tasks.getElementsByClassName("tasks-cards")[0];
 
-        numberTask += 1;
+            numberTask += 1;
 
-        localStorage.setItem(numberTask, JSON.stringify({"title":inputTitle, "about":inputAbout}));
+            localStorage.setItem(numberTask, JSON.stringify({"title":inputTitle.value, "about":inputAbout.value}));
 
-        tasksInfo.style.display = "none";
-        tasksCards.style.display = "flex";
+            tasksInfo.style.display = "none";
+            tasksCards.style.display = "flex";
 
-        newTask(JSON.parse(localStorage.getItem(numberTask)).title, JSON.parse(localStorage.getItem(numberTask)).about);
+            newTask(JSON.parse(localStorage.getItem(numberTask)).title, JSON.parse(localStorage.getItem(numberTask)).about);
+
+            inputTitle.value = "";
+            inputAbout.value = "";
+        }
+    }
+    
+    btnEdit.onclick = () => {
+        let numberTask = btnEdit.getAttribute("id");
+        let inputTitle = modalEdit.getElementsByTagName("input")[0];
+        let inputAbout = modalEdit.getElementsByTagName("textarea")[0];
+
+        let task = document.getElementsByClassName("task-card")[--numberTask];
+        let taskTitle = task.getElementsByTagName("h1")[0];
+        let taskP = task.getElementsByTagName("p")[0];
+
+        taskTitle.innerText = inputTitle.value;
+        taskP.innerText = inputAbout.value;
+
+        localStorage.setItem(++numberTask, JSON.stringify({"title":inputTitle, "about":inputAbout}));
+
+        inputTitle.value = "";
+        inputAbout.value = "";
+
+        isEdit = true;
+        cancel();
+    }
+
+    modal.onclick = (e) => {
+        if (isShare) {
+            if (e.target.getAttribute("class") != "modal-share" && e.target.getAttribute("class") != "share-section") {
+                console.log(e.target.getAttribute("class"));
+                
+                isShare = false;
+                modalShare.style.display = "none";
+                modal.style.display = "none";
+            }
+        }
+
+        if (isEdit) {
+            if (e.target.getAttribute("class") != "modal-edit" && e.target.getAttribute("class") != "modal-buttons" && e.target.getAttribute("class") != null) {
+                console.log(e.target.getAttribute("class"));
+                
+                isEdit = false;
+                modalEdit.style.display = "none";
+                modal.style.display = "none";
+            }
+        }
     }
 }
